3b846ccf9324d39cf76619ba42d383c54a2e066b
ALPONSIUSPOPODENSI <12200551@bsi.ac.id>
2021-10-06 16:49:11 +0700

    Tugas Pertemuan 3

--- app/Views/halaman/daftar_member.php
+++ app/Views/halaman/daftar_member.php
@@ -5,6 +5,10 @@
      <body>
          <h1>Hai.. kamu mau menjadi member?</h1>
          <p1>Isikan data diri kamu ya agar kami kenal dengan member kami</p1>
-     </body>
+     
+        </body>
 
 </html>
+
+
+
