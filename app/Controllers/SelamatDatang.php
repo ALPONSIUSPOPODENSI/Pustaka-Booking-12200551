f0b1b054e9d441886e1f91c9a607b006afeeba66
ALPONSIUSPOPODENSI <12200551@bsi.ac.id>
2021-11-22 21:44:39 +0700

    tugas pertemuan 8

--- app/Controllers/SelamatDatang.php
+++ app/Controllers/SelamatDatang.php
@@ -1,31 +1,32 @@
 <?php
 namespace App\Controllers;
 
-Class SelamatDatang extends BaseController {
-    public function hal_awal(){
-        return 'hello  saya belajar CI4';
+use Config\Services;
 
+class SelamatDatang extends BaseController {
+
+    public function hal_awal(){
+        return 'hello saya belajar CI4';
     }
-    public function beranda_login(){
+
+    public function beranda_login(){  
         return view('halaman/login', [
             'vd' => $this->session->getFlashdata('validator'),
             'email' => $this->session->get('email'),
             'sandi' => $this->session->get('sandi'),
-            'error' => $this->session->getFlashdata('error')
+            'error' => $this->session->getFlashdata('error'),
+            'title' => 'Login sisfo Perpustakaan'
         ]);
     }
 
     public function daftar_member(){
         return view('halaman/daftar_member');
-
     }
 
     public function hal_beranda(){
         return view('halaman/beranda', [
-           'email' => $this->session->get('email'),
-           'sandi' => $this->session->get('sandi')
-        ]);
+            'email' => $this->session->get('email'),
+            'sandi' => $this->session->get('sandi')
+        ]); 
     }
 }
-
-
